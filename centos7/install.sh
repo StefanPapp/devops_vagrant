2e9ac981b54fb7ee2cab19a42a31fc68e99d23c7
Stefan Papp <stefanpapp@Stefans-MacBook-Pro.local>
2019-12-23 13:02:03 +0400

    adjusted centos box

--- centos7/install.sh
+++ centos7/install.sh
@@ -8,4 +8,15 @@ hostnamectl --static set-hostname centos
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+git clone https://github.com/StefanPapp/devops_zshconf
+ln -s devops_zshconf/.zshrc ./zshrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+git clone https://github.com/sobolevn/sobole-zsh-theme.git
+ln -s $PWD/sobole-zsh-theme/sobole.zsh-theme ~/.oh-my-zsh/custom/themes/sobole.zsh-theme
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+  ~ZSH_CUSTOM/plugins/fast-syntax-highlighting
